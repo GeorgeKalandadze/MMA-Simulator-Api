430577cf5d3c77f50138a94e3377af50255af9ff
George Kalandadze <kalandadzeg777@gmail.com>
2024-05-28 14:27:24 +0400

    feat/make fighter model and migration

---
+++ database/migrations/2024_05_28_101731_create_fighters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fighters', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnDelete();
+            $table->integer('strength')->default(0);
+            $table->integer('agility')->default(0);
+            $table->integer('stamina')->default(0);
+            $table->integer('balance')->default(0);
+            $table->integer('height');
+            $table->integer('weight');
+            $table->foreignIdFor(\App\Models\WeightDivision::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\MartialArtStyle::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fighters');
+    }
+};
